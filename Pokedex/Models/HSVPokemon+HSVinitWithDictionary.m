961f0735ae52b7a3d0b099a50d272d23358e4eb8
hectorsvill <hectorstevenvillasano@gmail.com>
2020-04-25 20:16:16 -0700

    update HSVPokemon initWithDictionary

--- Pokedex/Models/HSVPokemon+HSVinitWithDictionary.m
+++ Pokedex/Models/HSVPokemon+HSVinitWithDictionary.m
@@ -12,9 +12,27 @@
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
-    NSString *name = dictionary[@"name"];
-    NSString *description = dictionary[@"description"];
-    NSNumber *pokemonID = [dictionary objectForKey:@"id"];
+    NSNumber *pokemonID                                     = [dictionary objectForKey:@"id"];
+    NSString *name                                          = [dictionary objectForKey:@"name"];
+    NSString *stage                                         = [dictionary objectForKey:@"stage"];
+    NSString *galar_dex                                     = [dictionary objectForKey:@"galar_dex"];
+    NSArray<NSNumber *> *base_stats                         = [dictionary objectForKey:@"base_stats"];
+    NSArray<NSNumber *> *ev_yield                           = [dictionary objectForKey:@"ev_yield"];
+    NSArray<NSString *> *abilities                          = [dictionary objectForKey:@"abilities"];
+    NSArray<NSString *> *types                              = [dictionary objectForKey:@"types"];
+    NSString *exp_grous                                     = [dictionary objectForKey:@"exp_group"];
+    NSArray<NSString *> *egg_groups                         = [dictionary objectForKey:@"egg_groups"];
+    NSNumber *hatch_cycles                                  = [dictionary objectForKey:@"weight"];
+    NSNumber *height                                        = [dictionary objectForKey:@"height"];
+    NSNumber *weight                                        = [dictionary objectForKey:@"weight"];
+    NSNumber *color                                         = [dictionary objectForKey:@"color"];
+    NSArray<NSDictionary *> *level_up_moves    = [dictionary objectForKey:@"level_up_moves"];
+
+    NSString *description           = [dictionary objectForKey:@"description"];
+
+
+
+
 
     return [self initWithName:name pokemonID:pokemonID descriptions: description];
 }
