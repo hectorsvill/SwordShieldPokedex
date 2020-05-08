5de01f527334a41183b9faa748572702e756cee2
hectorsvill <hectorstevenvillasano@gmail.com>
2020-05-08 13:06:57 -0700

    refactor HSVFavoritesViewController to collection view controller

--- Pokedex/View Controllers/HSVFavoriteCollectionViewController.h
+++ Pokedex/View Controllers/HSVFavoriteCollectionViewController.h
@@ -1,20 +1,17 @@
 //
-//  HSVFavoritesViewController.h
+//  HSVFavoriteCollectionViewController.h
 //  Pokedex
 //
-//  Created by Hector S. Villasano on 4/28/20.
+//  Created by Hector S. Villasano on 5/8/20.
 //  Copyright © 2020 s. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "HSVPokemonControllerProtocol.h"
 
-
-@class HSVPokemonController;
-
 NS_ASSUME_NONNULL_BEGIN
 
-@interface HSVFavoritesViewController : UIViewController<HSVPokemonControllerProtocol, UICollectionViewDataSource>
+@interface HSVFavoriteCollectionViewController : UICollectionViewController<HSVPokemonControllerProtocol>
 
 @property (nonatomic) HSVPokemonController *pokemonController;
 
---
+++ Pokedex/View Controllers/HSVFavoriteCollectionViewController.m
@@ -0,0 +1,67 @@
+//
+//  HSVFavoriteCollectionViewController.m
+//  Pokedex
+//
+//  Created by Hector S. Villasano on 5/8/20.
+//  Copyright © 2020 s. All rights reserved.
+//
+
+#import "HSVFavoriteCollectionViewController.h"
+#import "HSVPokemonController.h"
+#import "HSVPokemon.h"
+
+@interface HSVFavoriteCollectionViewController ()
+
+@property (nonatomic, copy) NSArray *favoriteIndexList;
+
+@end
+
+@implementation HSVFavoriteCollectionViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.pokemonController = HSVPokemonController.sharedPokemonController;
+    self.favoriteIndexList = [[self.pokemonController fetchFavorites] allObjects];
+
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    [self.collectionView reloadData];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    self.favoriteIndexList = [[self.pokemonController fetchFavorites] allObjects];
+    [self.collectionView reloadData];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [_favoriteIndexList count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = UIColor.redColor;
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+
+@end
