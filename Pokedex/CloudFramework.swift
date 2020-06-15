5814d3c433ecf5e55c5f60d89064ffd231ba59f4
hectorsvill <hectorstevenvillasano@gmail.com>
2020-06-15 15:04:21 -0700

    CloudFramework: createLeageCardRecord, save, fetchRecords

--- Pokedex/CloudFramework.swift
+++ Pokedex/CloudFramework.swift
@@ -8,26 +8,57 @@
 
 import Foundation
 import CloudKit
+
+
+@objc (HSVCloudFramework)
 class CloudFramework: NSObject {
-    let database = CKContainer.default().publicCloudDatabase
+    let container = CKContainer.default()
 
-    var leageCardRecords: [CKRecord] = []
-
-    func createReacord(with cardID: String, completion: @escaping (Error?) -> ()) {
-        let recordID = CKRecord.ID(recordName: UUID().uuidString)
-        let record = CKRecord(recordType: "LeageCardID", recordID: recordID)
-
-        record["cardID"] = cardID
-
-        database.save(record) { record, error in
+    /// Save a CKRecord
+    /// - Parameters:
+    ///   - record: CKRecord
+    ///   - completion: will return a error if save fails
+    /// - Returns: nill if record saves
+    @objc func save(record: CKRecord, completion: @escaping (Error?) -> ()) {
+        let database = container.publicCloudDatabase
+        database.save(record) { _, error in
             if let error = error {
                 completion(error)
             }
 
-            guard let record = record else { return }
-            self.leageCardRecords.append(record)
             completion(nil)
         }
     }
 
+    ///  perfroms fetch witih record Type Query
+    /// - Parameters:
+    ///   - recordType: a string value of the record typr
+    ///   - completion:  error / record data
+    @objc func fetchRecords(recordType: String, completion: @escaping ([CKRecord]?, Error?) -> ()) {
+        let query = CKQuery(recordType: recordType, predicate: NSPredicate(value: true))
+        let database = container.publicCloudDatabase
+
+        database.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                completion(nil, error)
+            }
+
+            guard let records = records else { return }
+            completion(records, nil)
+        }
+    }
+}
+
+
+extension CloudFramework {
+
+    /// Create a Leage Card ID CKRecord
+    /// - Parameter cardID: string value of ID
+    /// - Returns: a CKRecord  with cardID key/value
+    @objc func createLeageCardRecord(cardID: String) -> CKRecord{
+        let recordID = CKRecord.ID()
+        let record = CKRecord(recordType: "LeageCardID", recordID: recordID)
+        record["cardID"] = cardID
+        return record
+    }
 }
