d425ad89815130071811412285e0f3866a8b9f34
hectorsvill <hectorstevenvillasano@gmail.com>
2020-06-15 12:06:52 -0700

    CloudFramework.createReacord(with cardID

--- Pokedex/CloudFramework.swift
+++ Pokedex/CloudFramework.swift
@@ -9,10 +9,25 @@
 import Foundation
 import CloudKit
 class CloudFramework: NSObject {
+    let database = CKContainer.default().publicCloudDatabase
+
     var leageCardRecords: [CKRecord] = []
 
-    func createReacord(with cardID: String) {
+    func createReacord(with cardID: String, completion: @escaping (Error?) -> ()) {
+        let recordID = CKRecord.ID(recordName: UUID().uuidString)
+        let record = CKRecord(recordType: "LeageCardID", recordID: recordID)
 
+        record["cardID"] = cardID
+
+        database.save(record) { record, error in
+            if let error = error {
+                completion(error)
+            }
+
+            guard let record = record else { return }
+            self.leageCardRecords.append(record)
+            completion(nil)
+        }
     }
 
 }
