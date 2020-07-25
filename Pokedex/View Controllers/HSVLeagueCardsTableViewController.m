0a01e72e4de4b66797c92b977c63ad10931defbc
hectorsvill <hectorstevenvillasano@gmail.com>
2020-07-24 19:59:45 -0700

    refactor HSVLeagueCardsTableViewController to check for DEBUG

--- Pokedex/View Controllers/HSVLeagueCardsTableViewController.m
+++ Pokedex/View Controllers/HSVLeagueCardsTableViewController.m
@@ -48,12 +48,7 @@
     self.refreshControl = [UIRefreshControl new];
     [self.refreshControl addTarget:self action:@selector(fetchLeageCards) forControlEvents:UIControlEventValueChanged];
     [self.refreshControl setTintColor:UIColor.systemRedColor];
-    
-#if DEBUG
-    self.tableView.refreshControl = nil;
-#else
-    self.tableView.refreshControl = self.refreshControl;    
-#endif
+    self.tableView.refreshControl = (DEBUG) ? nil : self.refreshControl;
 }
 
 -(void)refresh {
