41b6ef3c9a7cc5af42bfb8960d9d5b7f3550d906
hectorsvill <hectorstevenvillasano@gmail.com>
2020-07-28 10:23:28 -0700

    NationalGalarPokedexUITestsError case searchPokemonSearchBarTappedFlowError

--- NationalGalarPokedexUITests/NationalGalarPokedexUITestsError.swift
+++ NationalGalarPokedexUITests/NationalGalarPokedexUITestsError.swift
@@ -18,4 +18,5 @@ enum NationalGalarPokedexUITestsError: Error {
     case leagueCardViewCellTappedError
     case addLeagueCardFlowError
     case AddLeagueCardResetButtonFlowError
+    case searchPokemonSearchBarTappedFlowError
 }
