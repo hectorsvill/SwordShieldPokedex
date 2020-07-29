2e4ea4703b12c3c8636cad81ab30ee7a28fa1fa8
hectorsvill <hectorstevenvillasano@gmail.com>
2020-07-29 10:59:56 -0700

    unused file

--- NationalGalarPokedexUITests/NationalPokemnNames.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NationalPokemnNames.swift
-//  NationalGalarPokedexUITests
-//
-//  Created by Hector Villasano on 7/28/20.
-//  Copyright © 2020 s. All rights reserved.
-//
-
-import Foundation
