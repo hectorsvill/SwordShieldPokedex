7ff3bcd6034b7c40e9a1790dba03a6629f920366
hectorsvill <hectorstevenvillasano@gmail.com>
2020-08-09 10:37:46 -0700

    NationalGalarPokedexUITests: LeagueCardID enum

---
+++ NationalGalarPokedexUITests/enums/LeagueCardID.swift
@@ -0,0 +1,13 @@
+//
+//  LeagueCardID.swift
+//  NationalGalarPokedexUITests
+//
+//  Created by Hector Villasano on 8/9/20.
+//  Copyright © 2020 s. All rights reserved.
+//
+
+import Foundation
+
+enum LeagueCardID: String, CaseIterable {
+    case id0 = "0000 0000 0000 00"
+}
